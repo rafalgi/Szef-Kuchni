fbabe46dfdd8954aeeafb64041b9240200e0692f
Rafał Gibała <141397136+rafalgi@users.noreply.github.com>
2024-12-01 20:13:42 +0100

    Delete Szef kuchni/MVVM/model directory

--- Szef kuchni/MVVM/model/Recipe.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public class Recipe
-{
-    // Przyk³adowa klasa reprezentuj¹ca przepis
-    public int Id { get; set; }
-    public string Title { get; set; }
-    public string SavePath { get; set; } // Œcie¿ka do obrazu
-}
