34d34f51c8676a9fe698c2e743fd2bdc463d4437
Rafał Gibała <rafal13996@gmail.com>
2024-12-01 20:22:52 +0100

    database connection

--- Szef kuchni/MVVM/ViewModel/MainViewModel.cs
+++ Szef kuchni/MVVM/ViewModel/MainViewModel.cs
@@ -1,82 +1,77 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Input;
 using Szef_kuchni.Core;
+using System;
+
 
 namespace Szef_kuchni.MVVM.ViewModel
 {
-   
     internal class MainViewModel : ObservableObject
     {
+        // Komendy aplikacji
         public ICommand CloseAppCommand { get; }
         public ICommand MinimizeAppCommand { get; }
         public RelayCommand HomeViewCommand { get; set; }
         public RelayCommand FavouriteViewCommand { get; set; }
         public RelayCommand SearchViewCommand { get; set; }
         public RelayCommand HistoryViewCommand { get; set; }
-        
 
-
-        public HomeViewModel HomeVM { get; set; }   
+        // ViewModels
+        public HomeViewModel HomeVM { get; set; }
         public FavouriteViewModel FavouriteVM { get; set; }
         public SearchViewModel SearchVM { get; set; }
         public HistoryViewModel HistoryViewModel { get; set; }
 
+        // Aktualnie wyświetlany widok
         private object _currentView;
-
         public object CurrentView
         {
-            get { return _currentView; }
+            get => _currentView;
             set
             {
                 _currentView = value;
                 OnPropertyChanged();
             }
-               
         }
 
+        // Wybrany obraz
+        private string _selectedImage;
+        public string SelectedImage
+        {
+            get => _selectedImage;
+            set
+            {
+                _selectedImage = value;
+                OnPropertyChanged();
+            }
+        }
+
+        // Konstruktor
         public MainViewModel()
         {
+            // Inicjalizacja ViewModels
             HomeVM = new HomeViewModel();
-            CurrentView = HomeVM;
             FavouriteVM = new FavouriteViewModel();
             SearchVM = new SearchViewModel();
             HistoryViewModel = new HistoryViewModel();
+            CurrentView = HomeVM;
 
-            HomeViewCommand = new RelayCommand(o =>
-            {
-                CurrentView = HomeVM;
-            });
-
-            FavouriteViewCommand = new RelayCommand(o =>
-            {
-                CurrentView = FavouriteVM;
-            });
-
-            SearchViewCommand = new RelayCommand(o =>
-            {
-                CurrentView = SearchVM;
-            });
-
-
-            HistoryViewCommand = new RelayCommand(o =>
-            {
-                CurrentView = CurrentView;
-            });
-
+            // Inicjalizacja komend
+            HomeViewCommand = new RelayCommand(o => CurrentView = HomeVM);
+            FavouriteViewCommand = new RelayCommand(o => CurrentView = FavouriteVM);
+            SearchViewCommand = new RelayCommand(o => CurrentView = SearchVM);
+            HistoryViewCommand = new RelayCommand(o => CurrentView = HistoryViewModel);
             CloseAppCommand = new RelayCommand(ExecuteCloseApp);
             MinimizeAppCommand = new RelayCommand(ExecuteMinimizeApp);
         }
 
+        // Metoda do zamknięcia aplikacji
         private void ExecuteCloseApp(object obj)
         {
-            System.Windows.Application.Current.Shutdown();
+            Application.Current.Shutdown();
         }
 
+        // Metoda do minimalizacji okna
         private void ExecuteMinimizeApp(object obj)
         {
             Application.Current.MainWindow.WindowState = WindowState.Minimized;
