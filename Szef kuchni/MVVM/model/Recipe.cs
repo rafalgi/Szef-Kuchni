0e07cab4029e9a63d18001dea55a302e9f4805aa
Rafał Gibała <141397136+rafalgi@users.noreply.github.com>
2024-12-01 13:12:26 +0100

    add model files

---
+++ Szef kuchni/MVVM/model/Recipe.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Recipe
+{
+    // Przyk³adowa klasa reprezentuj¹ca przepis
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string SavePath { get; set; } // Œcie¿ka do obrazu
+}
