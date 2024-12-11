ff58596de2798803f9fafc6be262d0c84f655e64
Gipsonek123 <134952200+Gipsonek123@users.noreply.github.com>
2024-12-11 23:30:31 +0100

    "Przeglądaj przepisy" - back button

--- Szef kuchni/MVVM/ViewModel/MainViewModel.cs
+++ Szef kuchni/MVVM/ViewModel/MainViewModel.cs
@@ -1,9 +1,8 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Input;
 using Szef_kuchni.Core;
-using System;
 using Szef_kuchni.MVVM.View;
-
 
 namespace Szef_kuchni.MVVM.ViewModel
 {
@@ -12,6 +11,8 @@ namespace Szef_kuchni.MVVM.ViewModel
         public ICommand CloseAppCommand { get; }
         public ICommand MinimizeAppCommand { get; }
         public ICommand OpenRecipeCommand { get; }
+        public ICommand GoBackCommand { get; }
+        
         public RelayCommand HomeViewCommand { get; set; }
         public RelayCommand FavouriteViewCommand { get; set; }
         public RelayCommand SearchViewCommand { get; set; }
@@ -22,8 +23,9 @@ namespace Szef_kuchni.MVVM.ViewModel
         public SearchViewModel SearchVM { get; set; }
         public HistoryViewModel HistoryViewModel { get; set; }
 
-        // aktualnie wyświetlany widok
         private object _currentView;
+        private readonly Stack<object> _viewHistory = new Stack<object>();
+
         public object CurrentView
         {
             get => _currentView;
@@ -34,18 +36,6 @@ namespace Szef_kuchni.MVVM.ViewModel
             }
         }
 
-        private string _selectedImage;
-        public string SelectedImage
-        {
-            get => _selectedImage;
-            set
-            {
-                _selectedImage = value;
-                OnPropertyChanged();
-            }
-        }
-
-        //konstruktor
         public MainViewModel()
         {
             HomeVM = new HomeViewModel();
@@ -58,35 +48,47 @@ namespace Szef_kuchni.MVVM.ViewModel
             FavouriteViewCommand = new RelayCommand(o => CurrentView = FavouriteVM);
             SearchViewCommand = new RelayCommand(o => CurrentView = SearchVM);
             HistoryViewCommand = new RelayCommand(o => CurrentView = HistoryViewModel);
+            
             CloseAppCommand = new RelayCommand(ExecuteCloseApp);
             MinimizeAppCommand = new RelayCommand(ExecuteMinimizeApp);
-
+            
             OpenRecipeCommand = new RelayCommand(OpenRecipe);
+            GoBackCommand = new RelayCommand(GoBack);
         }
 
-        // zamykanie aplikacji na X
         private void ExecuteCloseApp(object obj)
         {
             Application.Current.Shutdown();
         }
 
-        // minimalizacja aplikacji na -
         private void ExecuteMinimizeApp(object obj)
         {
             Application.Current.MainWindow.WindowState = WindowState.Minimized;
         }
-        // otwarcie nowego okna z dokładniejszym przepisem
+
         private void OpenRecipe(object parameter)
         {
             if (parameter is int recipeId)
             {
+                _viewHistory.Push(CurrentView); // Zapisz obecny widok
                 CurrentView = new RecipeDetailsWindow(recipeId);
             }
             else
             {
-                MessageBox.Show("Nie udało się rozpoznać ID przepisu.");  // sprawdzenie 
+                MessageBox.Show("Nie udało się rozpoznać ID przepisu.");
             }
         }
 
+        private void GoBack(object parameter)
+        {
+            if (_viewHistory.Count > 0)
+            {
+                CurrentView = _viewHistory.Pop(); // Pobierz poprzedni widok
+            }
+            else
+            {
+                MessageBox.Show("Brak poprzednich widoków w historii.");
+            }
+        }
     }
 }
